794b2919faffd9320800565d0e1f4e086def82b7
insoooooooooJang <oasisholics@gmail.com>
2021-07-23 18:51:42 +0900

    controller를 이용한 api 테스트와 lombok 적용 후 api 테스트

---
+++ src/test/java/com/insoo/jang/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.insoo.jang.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트(){
+        //given
+        String name="test";
+        int amount=1000;
+
+        // 롬복으로 자동으로 생성자 만들어준 걸로 객체 생성
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //저 객체 만들때 set한 값이 지금 name, amount 하고 같은지 체크 (lombok이 getter, setter 잘 만드는지 확인함)
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
