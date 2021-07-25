3d046ac043331257f46c51f0fd2c345295863f50
insoooooooooJang <oasisholics@gmail.com>
2021-07-25 18:25:13 +0900

    post 수정 기능 구현 / 테스트 코드 작성 JPA createTime, modifyTime 추가

--- src/test/java/com/insoo/jang/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/insoo/jang/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.insoo.jang.springboot.web;
 import com.insoo.jang.springboot.domain.posts.Posts;
 import com.insoo.jang.springboot.domain.posts.PostsRepository;
 import com.insoo.jang.springboot.web.dto.PostsSaveRequestDto;
+import com.insoo.jang.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -59,5 +62,38 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void Posts_수정된다() throws  Exception{
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                                            .title("title")
+                                            .content("content")
+                                            .author("author")
+                                            .build()); // db에 posts 저장
+        Long updateId = savedPosts.getId(); // 저장 한 뒤 해당 row의 id 가져오기
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                                                                .title(expectedTitle)
+                                                                .content(expectedContent)
+                                                                .build(); // 업데이트 requestDto 생성
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId; // 요청 url
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class); // 요청
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK); //상태 체크
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll(); // db 에 저장된 거 다 가져옴
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle); // 1번째 열의 제목, 콘텐츠가 요청한 내용대로 변경되었는지 (update 잘 되었는지) 확인
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 }
