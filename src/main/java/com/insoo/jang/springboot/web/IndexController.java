a465592ca123026f0b1b839e04b0551deaafd3c5
insoooooooooJang <oasisholics@gmail.com>
2021-08-18 06:20:07 +0900

    naver로그인시 index페이지로 가지 않고 에러 페이지로 빠져서 Controller annotation을 restController 로 변경해봄

--- src/main/java/com/insoo/jang/springboot/web/IndexController.java
+++ src/main/java/com/insoo/jang/springboot/web/IndexController.java
@@ -10,11 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
