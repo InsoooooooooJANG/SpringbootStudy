703358fe2209fba224694c55358389375c4965d5
insoooooooooJang <oasisholics@gmail.com>
2021-08-18 06:04:55 +0900

    application.properties - real db 사용하지 않도록 수정 Application.java - naver 로그인 시 에러페이지로 이동해서 임시방편으로 추가

--- src/main/java/com/insoo/jang/springboot/Application.java
+++ src/main/java/com/insoo/jang/springboot/Application.java
@@ -2,10 +2,12 @@ package com.insoo.jang.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan(basePackages = ("web"))
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
