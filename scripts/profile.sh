d68972b504995149e3ea7bfabdc78de9b827386b
insoooooooooJang <oasisholics@gmail.com>
2021-08-22 00:45:51 +0900

    1. 배포스크립트/스위칭/확인 스크립트 차례로 돌도록 appspec 추가 2. 스크립트 추가 3. 버전에 날짜붙이도록 gradle 수정

---
+++ scripts/profile.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#쉬고있는 profile을 찾는다. real1 이 사용 중이면 2가 쉬고 있는 거고, 2가 사용중이면 1이 쉬고있는거
+
+function find_idle_profile()
+{
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+  if [ ${RESPONSE_CODE} -ge 400 ]
+  then
+    CURRENT_PROFILE=real2
+  else
+    CURRENT_PROFILE=$(curl -s http://localhost/profile)
+  fi
+
+  if [ ${CURRENT_PROFILE} == real1 ]
+  then
+    IDLE_PROFILE=real2
+  else
+    IDLE_PROFILE=real1
+  fi
+
+  echo "${IDLE_PROFILE}"
+}
+
+#쉬고있는 prifle의 port 가져오기
+function find_idle_port()
+{
+  IDLE_PROFILE=$(find_idle_profile)
+
+  if [ ${IDLE_PROFILE} == real1 ]
+  then
+    echo "8081"
+  else
+    echo "8082"
+  fi
+}
